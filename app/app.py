05c5d94f925d8c6d545b59d0eb8cd3df1cf1bf36
Reuben Britto <reubenbritto@Reubens-MBP-2.attlocal.net>
2020-11-30 15:21:57 -0800

    flask update

--- app/app.py
+++ app/app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+app.vars = {}
+
+@app.route('/', methods=['GET','POST'])
 def index():
-  return render_template('index.html', num=1)
+	if request.method == 'GET':
+		return render_template('index.html', num=1)
+	elif request.method == 'POST':
+		app.vars['stock_ticker'] = request.form['stock_ticker_input']
+		return redirect('/plot')
+
+@app.route('/plot', methods=['GET','POST'])
+def plot():
+	return render_template('plot.html', stock=app.vars['stock_ticker'])
+
+
 
 if __name__ == '__main__':
   app.run(port=33507)
